9f20fccaf5718d375acec4f2ee3c64a1f512bcdc
Thomas Taylor <thomas.taylor@kineo.com>
2019-06-05 12:10:06 +0100

    Amend doc line

--- coreplugins-plugin.js
+++ coreplugins-plugin.js
@@ -5,7 +5,7 @@ const pkg = require(path.join(process.cwd(), 'package.json'));
 
 class Plugin {
   onHandleConfig(ev) {
-    let output = `# List of core modules\n**The following modules are included in Adapt authoring v${pkg.version}, and supported by the core dev team:**\n\n| Name | Version | Description |\n| ---- | ------- | ----------- |\n`;
+    let output = `# List of core modules\n**The following modules are included in Adapt authoring v${pkg.version}, and supported by the core dev team.**\n\nThe 'Module' column refers to whether the dependency is booted as an authoring tool module, or simply used as a regular Node dependency.\n\n| Name | Version | Module | Description |\n| ---- | :-----: | :----: | ----------- |\n`;
     Object.entries(Object.assign({}, pkg.dependencies, pkg.devDependencies)).sort((a,b) => {
       if(a[0] < b[0]) return -1;
       if(a[0] > b[0]) return 1;
