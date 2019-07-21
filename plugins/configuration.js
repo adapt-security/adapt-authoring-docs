7790bd79314fc8680b21042f5b8c0ff9748a92a3
Thomas Taylor <thomas.taylor@kineo.com>
2019-07-21 08:57:51 +0100

    Update plugin

--- plugins/configuration.js
+++ plugins/configuration.js
@@ -2,28 +2,55 @@ const fs = require('fs-extra');
 const path = require('path');
 
 const pkg = require(path.join(process.cwd(), 'package.json'));
+const schemas = [];
 
 class Plugin {
   onHandleConfig(ev) {
-    let output = '\`\`\`javascript\nmodule.exports = {\n';
-    Object.entries(Object.assign({}, pkg.dependencies, pkg.devDependencies)).forEach(([dep, v]) => {
-      let schema
+    this.loadSchemas();
+    this.writeFile({
+      'CODE_EXAMPLE': this.generateCodeExample(),
+      'LIST': this.generateList()
+    });
+  }
+  loadSchemas() {
+    Object.keys(Object.assign({}, pkg.dependencies, pkg.devDependencies)).forEach(dep => {
       try {
-        schema = require('.' + path.join(process.cwd(), 'node_modules', dep, 'conf', 'config.schema.js')).definition;
+        schemas[dep] = require('.' + path.join(process.cwd(), 'node_modules', dep, 'conf', 'config.schema.js')).definition;
       } catch(e) {
         return;
       }
+    });
+  }
+  generateCodeExample() {
+    let output = '\`\`\`javascript\nmodule.exports = {\n';
+
+    Object.entries(schemas).forEach(([dep, schema]) => {
       output += `  '${dep}': {\n`;
       Object.entries(schema).forEach(([attr, config]) => {
         if(config.description) output += `    // ${config.description}\n`;
         if(config.help) output += `    // ${config.help}\n`;
-        output += `    ${attr}: ${this.defaultToMd(config)} // (${config.type}) ${config.required ? 'required' : 'optional'}\n`;
+        output += `    ${attr}: ${this.defaultToMd(config)} // ${config.type}, ${config.required ? 'required' : 'optional'}\n`;
       });
       output += `  }\n`;
     });
     output += `};\n\`\`\``;
 
-    this.writeFile(output);
+    return output;
+  }
+  generateList() {
+    let output = '';
+
+    Object.entries(schemas).forEach(([dep, schema]) => {
+      output += `### ${dep}\n\n`
+      Object.entries(schema).forEach(([attr, config]) => {
+        output += `**${attr}** (${config.type || ''}, ${config.required ? 'required' : 'optional'})<br>`;
+        output += `${config.description}\n\n`;
+        output += `Default: \`${this.defaultToMd(config)}\`\n\n`;
+      });
+      output += `***\n\n`;
+    });
+
+    return output;
   }
   /**
   * Returns a string formatted nicely for markdown
@@ -42,10 +69,10 @@ class Plugin {
     }
   }
 
-  writeFile(content) {
-    const input = fs.readFileSync(path.join(__dirname, '..', 'docspartials', 'configuration.md')).toString();
-    const output = input.replace('{{{REPLACE_ME}}}', content);
-    fs.writeFileSync(path.join(__dirname, '..', 'docs', 'temp-configuration.md'), output);
+  writeFile(data) {
+    let file = fs.readFileSync(path.join(__dirname, '..', 'docspartials', 'configuration.md')).toString();
+    Object.entries(data).forEach(([key,value]) => file = file.replace(`{{{${key}}}}`, value));
+    fs.writeFileSync(path.join(__dirname, '..', 'docs', 'temp-configuration.md'), file);
   }
 }
 
