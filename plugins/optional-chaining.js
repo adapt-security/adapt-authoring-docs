2cdf247a422e5ce4d98b0bf7412d8bb464092870
Tom Taylor <hello@tomtaylor.name>
2021-07-15 19:39:13 +0100

    Fix replace

--- plugins/optional-chaining.js
+++ plugins/optional-chaining.js
@@ -2,11 +2,9 @@
  * Removes any optional chaining use from the incoming doc
  * @see https://github.com/esdoc/esdoc/issues/532#issuecomment-670843906
  */
-class Plugin {
+ class Plugin {
   onHandleCode(event) {
-    event.data.code = event.data.code.replace(/\w+\?\.\b/g, substr => {
-      return substr.replace(/\?/g, '');
-    });
+    event.data.code = event.data.code.replace(/\w+\?\./g, s => s.replace(/\?/g, ''));
   }
 }
 
