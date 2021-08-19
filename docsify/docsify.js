e605e78d1289359bebcf4d1c5e6db3f570b72b5a
Tom Taylor <hello@tomtaylor.name>
2021-08-19 19:08:40 +0100

    Fix sidebar sorting and filtering

--- docsify/docsify.js
+++ docsify/docsify.js
@@ -8,32 +8,45 @@ const execPromise = promisify(require('child_process').exec);
 /**
  * Copies all doc files ready for the generator
  */
-async function docsify(configs, outputdir, manualIndex) {
-  let sidebarMd = '';
-  const files = configs.reduce((allFiles, c) => {
-    const docFiles = glob.sync('docs/*.md', { cwd: c.rootDir, absolute: true });
-    if(docFiles.length) {
-      // sidebarMd += `* ${c.name}\n`;
-      docFiles.forEach(f => {
-        let title = path.basename(f);
-        try {
-          title = fs.readFileSync(f).toString().match(/^#(?!#)\s?(.*)/)[1];
-        } catch(e) {}
-        // sidebarMd += `  * [${title}](${path.basename(f)})\n`;
-        sidebarMd += `* [${title}](${path.basename(f)})\n`;
-      });
-    }
-    return allFiles.concat(docFiles);
-  }, []);
-  const dir = path.resolve(`${outputdir}/docsify`);
+async function docsify(configs, outputdir, manualIndex, sourceIndex) {
+  const titleMap = configs.reduce((m,c) => {
+    glob.sync('docs/*.md', { cwd: c.rootDir, absolute: true }).forEach(f => {
+      if(f === sourceIndex) {
+        return;
+      }
+      let title = path.basename(f);
+      try { title = fs.readFileSync(f).toString().match(/^#(?!#)\s?(.*)/)[1]; } catch(e) {}
+      m[title] = f;
+    });
+    return m;
+  }, {});
+  const dir = `${outputdir}/docsify`;
+  /**
+   * init docsify folder
+   */
   await execPromise(`npx docsify init ${dir}`);
-  await fs.writeFile(`${dir}/_sidebar.md`, sidebarMd);
+  /**
+   * Copy files
+   */
   await fs.copy(`${__dirname}/index.html`, `${dir}/index.html`);
   await fs.copy(`${__dirname}/styles`, `${dir}/styles`);
   if(manualIndex) {
     await fs.copy(manualIndex, `${dir}/_coverpage.md`);
   }
-  await Promise.all(files.map(f => fs.copy(f, `${dir}/${path.basename(f)}`)));
+  await Promise.all(Object.values(titleMap).map(f => fs.copy(f, `${dir}/${path.basename(f)}`)));
+  /**
+   * Generate custom sidebar
+   */
+  let sidebarMd = '';
+  Object.keys(titleMap)
+    .sort((a,b) => a.localeCompare(b))
+    .forEach(n => {
+      if(titleMap[n] !== manualIndex) {
+        sidebarMd += `* [${n}](${path.basename(titleMap[n])})\n`;
+      }
+    });
+
+  await fs.writeFile(`${dir}/_sidebar.md`, sidebarMd);
 }
 
 module.exports = docsify;
