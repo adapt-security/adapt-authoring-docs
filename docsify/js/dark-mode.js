d17fb596d7bffec2f17ff85ccc973da5f1025aad
Thomas Taylor <thomas.taylor@kineo.com>
2022-10-26 22:40:02 +0100

    Add docsify dark mode theme overrides

--- docsify/js/dark-mode.js
+++ docsify/js/dark-mode.js
@@ -9,5 +9,7 @@ addEventListener('load', () => {
     const darkMode = localStorage.getItem('darkMode') === 'true';
     document.getElementById("light-theme").disabled = darkMode;
     document.getElementById("dark-theme").disabled = !darkMode;
+    const classList = document.getElementsByTagName("body")[0].classList;
+    darkMode ? classList.add('dark') : classList.remove('dark');
   }
 });
