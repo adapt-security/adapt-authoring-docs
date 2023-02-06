08120852cabfb116bc0479d561acda9371b3994e
Thomas Taylor <hello@tomtaylor.name>
2023-02-06 20:46:05 +0000

    Refactor

--- swagger/index.js
+++ swagger/index.js
@@ -1,10 +1,2 @@
 import SwaggerUI from 'swagger-ui'
-// import 'swagger-ui/dist/swagger-ui.css';
-
-addEventListener("load", async () => {
-  SwaggerUI({ 
-    url: '/api.json',
-    dom_id: '#swagger'
-  });
-});
-
+addEventListener("load", () => SwaggerUI({ url: '/api.json', dom_id: '#swagger' }));
