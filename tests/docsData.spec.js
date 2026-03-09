c5541f5d180ef27e7d7bedc2fd1a5d2e399792ff
Thomas Taylor <thomas.taylor@kineo.com>
2026-03-09 22:25:36 +0000

    Fix: Remove extra blank line to satisfy linter

--- tests/docsData.spec.js
+++ tests/docsData.spec.js
@@ -168,7 +168,6 @@ async function createFixture () {
     }
   }))
 
-
   // --- fake docs module with config ---
   const docsDir = path.join(root, 'node_modules', 'adapt-authoring-docs')
   await fs.mkdir(path.join(docsDir, 'conf'), { recursive: true })
